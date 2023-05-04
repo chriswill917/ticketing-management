ec4ada260de82d55a967d998156adec583268116
Chis Williams <willchris917@gmail.com>
2023-05-04 08:19:05 +0530

    Fixed code

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -6,7 +6,7 @@ const buildClient = ({ req }) => {
 
     return axios.create({
       baseURL:
-        "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local",
+        "http://www.ticketing-test.pro/",
       headers: req.headers,
     });
   } else {
