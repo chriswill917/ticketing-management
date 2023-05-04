7fba68d2d31ae5594d49c06898c51795503c246a
Chis Williams <willchris917@gmail.com>
2023-05-04 09:16:57 +0530

    update url

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -3,10 +3,11 @@ import axios from "axios";
 const buildClient = ({ req }) => {
   if (typeof window === "undefined") {
     // We are on the server
-
+     console.log('nin.......')
+     console.log('n,,,,,')
     return axios.create({
       baseURL:
-        "http://www.ticketing-test.pro/",
+        "http://www.ticketing-test.pro",
       headers: req.headers,
     });
   } else {
